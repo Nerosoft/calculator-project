fed8448c483691f35ca14217a201b3864cd00fbb
Nerosoft <ggamals7744@yahoo.com>
2024-05-02 04:21:12 +0300

    Create InitObject.hh

---
+++ InitObject.hh
@@ -0,0 +1,15 @@
+class InitObject {
+protected:
+	std::vector<std::vector<Input>> the_result;
+	Table table;
+	Colors color;
+	Main main;
+	Dialog dialog;
+	Help help;
+	Confirm confirm;
+	Delete m_delete;
+	Json json;
+	AddSearchEdit add;
+	AddSearchEdit search;
+	AddSearchEdit edit;
+};
