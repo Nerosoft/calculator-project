115e9fc790a488a09c8b4a48619a85c4dfc5c3bd
Nerosoft <ggamals7744@yahoo.com>
2024-06-12 01:10:09 +0300

    Update Section.hh

--- Section.hh
+++ Section.hh
@@ -15,16 +15,4 @@ public:
 		input_mark_search,
 		input_mark_update_add
 	};
-
-
-	enum class InputKey {
-		addition,
-		search,
-		edit,
-		deletion,
-		first_call_add,
-		first_call_search,
-		first_call_edit,
-		confirm
-	};
 };
